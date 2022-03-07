e4649b4dd5fd119fc01854ede2ff3bae77bd148e
gord-larisa <gord-larisa@yandex.ru>
2022-03-07 18:24:05 +0200

    hw1

--- src/test/java/StartPhoneBook.java
+++ src/test/java/StartPhoneBook.java
@@ -18,9 +18,18 @@ public class StartPhoneBook {
         //steps of test login
 
         // open login form ------find loginbtn+click------> open form login
-        WebElement el = wd.findElement(By.id("idel"));
-        List<WebElement> list=wd.findElements(By.id("idel"));
-        
+        //WebElement el = wd.findElement(By.id("idel"));// example
+        //List<WebElement> list=wd.findElements(By.id("idel"));//example
+
+        List<WebElement> Elements = wd.findElements(By.tagName("a"));
+        WebElement login = Elements.get(2);
+        login.click();
+
+        WebElement element = wd.findElement(By.id("root"));
+        WebElement element1 = wd.findElement(By.className("container"));
+
+
+
         //fill email      -------find email element+click+clear+type email
         //fill password   -------find password element+click+clear+clear+type password
         //click login btn -------find login element+click
@@ -34,7 +43,24 @@ public class StartPhoneBook {
 
 
         //wd.close()// close one tab
-        wd.quit();//close all tabs
+       // wd.quit();//close all tabs
+
+
+
+        //home work
+        @Test
+        public void startPhoneBook(){
+            wd=new ChromeDriver();
+            wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/login");//open www+history of steps
+            wd.manage().window().maximize();
+
+
+            WebElement element = wd.findElement(By.id("root"));
+            WebElement element1 = wd.findElement(By.className("container"));
+
+
+
+            
 
     }
 }
